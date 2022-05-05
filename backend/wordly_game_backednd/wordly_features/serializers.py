1f8c46a1df23ae7ba0850d0947fc0d9541a950fb
Nadia Tumareva <tumarevanadya@gmail.com>
2022-05-04 22:19:59 -0400

    Serializesrs  were created

--- backend/wordly_game_backednd/wordly_features/serializers.py
+++ backend/wordly_game_backednd/wordly_features/serializers.py
@@ -1,5 +1,8 @@
+from ast import IsNot
+from imp import source_from_cache
 from rest_framework import serializers
-from .models import Words, UserWord
+from .models import DayChallenge, Words, UserWord
+from datetime import datetime
 
 
 class WordSerializer(serializers.ModelSerializer):
@@ -9,25 +12,36 @@ class WordSerializer(serializers.ModelSerializer):
         fields = ('id', 'word', 'is_active')
 
 
+class DayChallengeSerializer(serializers.ModelSerializer):
+    '''Serializer for DayChallenge.'''
+    class Meta:
+        model = DayChallenge
+        fields = ('id', 'word', 'date')
+
+
 class WordInputSerializer(serializers.ModelSerializer):
     '''Serializer for UserWord'''
-    player = serializers.SlugRelatedField(
-        slug_field="user_token",
-        read_only=True,
+    player = serializers.ReadOnlyField(
+        source_from_cache="user_token",
+        read_only=True
+    )
+    task = serializers.ReadOnlyField(
+        source=DayChallenge.objects.filter(
+            date=datetime.now().strftime('%Y-%m-%d'))
     )
 
     class Meta:
         model = UserWord
-        fields = ("id", "word", "player")
+        fields = ("id", "word", "task", "player", "attempt")
 
     def validate(self, attrs):
-        """Check that the player can enter only the word
+        """Checks that the player can enter only the word
         that is in the database.
         """
         if not self.context["request"].method == "POST":
             return attrs
-        if Words.objects.filter(
-                word=self.context["view"].kwargs.get("word")).exists():
+        if IsNot(Words.objects.filter(
+                word=self.context["view"].kwargs.get("word")).exists()):
             raise serializers.ValidationError(
                 (
                     "There is not such word in the database."
--- backend/wordly_game_backednd/wordly_features/views.py
+++ backend/wordly_game_backednd/wordly_features/views.py
@@ -1,11 +1,62 @@
-from .models import Score
-from rest_framework import generics, permissions
+import random
+
+from django.shortcuts import get_object_or_404
+
+from .serializers import DayChallengeSerializer, WordInputSerializer
+from .models import Words, DayChallenge
+from rest_framework import generics, permissions, response, status
 from rest_framework.decorators import action
 
 
-class ScoreViewSet(generics.CreateAPIView):
+def random_word():
+    length = len(Words.objects.all()) - 1
+    index_word = random.randint(0, length)
+    return Words.objects.get(id=index_word)
+
+
+def del_word(self, request, pk):
+    word = get_object_or_404(DayChallenge, user=request.user, recipe=pk)
+    word.delete()
+    return response.Response(
+        status=status.HTTP_204_NO_CONTENT)
+
+
+def daily_word():
+    while True:
+        try:
+            pk_word = random_word()
+            day_challenge = get_object_or_404(Words, pk=pk_word)
+            DayChallenge.objects.create(word=day_challenge)
+        except Exception as inst:
+            raise Exception(f'Somthing wrong happended {inst}')
+
+
+@action(detail=True, methods=['POST', 'DELETE'],
+        url_path='day_challenge',
+        permission_classes=[permissions.IsAdminUser],)
+def day_word(request, pk):
+    if request.method == 'POST':
+        serializer = DayChallengeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response(
+                serializer.data, status=status.HTTP_201_CREATED)
+        return response.Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        return del_word(request, pk)
+
+
+class UserWordViewSet(generics.CreateAPIView):
     permission_classes = (permissions.AllowAny,)
+    serializer_class = WordInputSerializer
 
-    def post(self, request):
+    def perform_create(self, serializer):
+        word = serializer.data['word']
+        player = serializer.data['player']
+        task = 
+        attempt = 
 
-        return super().post(request)
+        return super().perform_create(serializer)
+
+
